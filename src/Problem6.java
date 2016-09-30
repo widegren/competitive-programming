cff03081651665f3b5ba8fe6179fa3d08dc9588a
Emelie Widegren <emelie.widegren@chalmers.se>
2016-09-30 22:32:09 +0200

    Problem6

---
+++ src/Problem6.java
@@ -0,0 +1,18 @@
+
+public class Problem6 {
+	
+	public static void main(String arg[]) {
+		problem6();
+	}
+	
+	public static void problem6() {
+		int res = 0, res1 = 0, square;
+		for (int i = 1; i < 101; i++) {
+			res = res + i * i;
+			res1 = res1 + i;
+		}
+		res1 = res1 * res1;
+		square = res1 - res;
+		System.out.println(square);
+	}
+}
