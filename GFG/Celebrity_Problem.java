9cab37c659f2d673b6a0e54aa477233b252dcde1
Emelie Widegren <emelie.widegren@chalmers.se>
2016-10-19 19:12:34 +0200

    Celebrity Problem
    You are in a party of N people, where only one person is known to
    everyone. Such a person may be present in the party, if yes, (s)he
    doesn’t know anyone in the party. Your task is to find the stranger
    (celebrity) in party.
    You will be given a square matrix M where if an element of row i and
    column j  is set to 1 it means there is an edge from ith person to jth
    person. An edge represent the relation that i th person knows j th
    person. You need to complete the function getId which finds the id of
    the celebrity if present else return -1. The function getId takes two
    arguments the square matrix M and its size n.

---
+++ GFG/Celebrity_Problem.java
@@ -0,0 +1,52 @@
+package GFG;
+
+import java.util.*;
+
+public class Celebrity_Problem {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t > 0) {
+			int N = sc.nextInt();
+			int M[][] = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+					M[i][j] = sc.nextInt();
+				}
+			}
+			System.out.println(getId(M, N));
+			t--;
+		}
+		sc.close();
+	}
+
+	static int getId(int M[][], int n) {
+		int left = 0;
+        int right = n - 1;
+         
+        // Step 1: Find the potential candidate
+        while (left < right) {
+            if (haveAcquaintance(M[left], right)) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+         
+        // Step 2: Validate the candidate
+        int candidate = right;
+        for (int i = 0; i < n; i++) {
+            if (i != candidate && (!haveAcquaintance(M[i], candidate) || haveAcquaintance(M[candidate], i))) {
+                return -1;
+            }
+        } 
+        return candidate;
+	}
+	
+	static boolean haveAcquaintance(int[] M, int index){
+		if(M[index] == 1)
+			return true;
+		else
+			return false;
+	}
+}
