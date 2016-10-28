621b87f17e84ff9bbb33ad55b79d8725a3b97328
Emelie Widegren <emelie.widegren@chalmers.se>
2016-10-28 13:55:40 +0200

    Longest path
    http://www.geeksforgeeks.org/longest-path-between-any-pair-of-vertices/
    
    works for the given test cases but fails for:
    1
    94 87
    10 20 794 32 11 493 58 74 363 20 63 60 84 71 541 79 17 737 60 57 568 10
    3 531 27 56 68 20 6 803 67 55 70 52 2 457 80 59 230 82 34 920 21 74 199
    45 24 371 18 83 92 1 2 874 1 2 997 74 10 926 33 76 337 6 83 730 74 78
    125 4 31 546 39 52 435 57 52 751 8 25 277 15 33 585 64 24 755 44 13 61
    69 1 740 65 47 587 19 36 796 71 39 379 72 34 98 63 46 493 77 9 302 45 63
    442 22 42 445 72 41 730 4 26 98 12 46 676 58 20 568 53 6 354 79 78 307
    72 12 625 29 4 733 54 4 20 67 25 709 60 53 150 61 12 619 62 63 452 26 36
    380 65 45 229 14 39 194 77 27 765 61 11 988 9 64 492 60 2 860 29 17 552
    78 77 276 24 55 122 15 80 30 26 16 794 39 69 144 4 61 530 21 77 444 80
    42 539 15 17 905 59 1 689 70 34 918 49 57 744 7 72 491 84 53 726 77 7
    506 24 51 787 82 27 543 21 62 508 64 77 349 24 51 829 84 76 747 9 41 423
    72 47 606 1 2 731 71 78 321 81 17 627 67 54 709

--- gfg/LongestPath.java
+++ gfg/LongestPath.java
@@ -1,6 +1,10 @@
 package gfg;
 
+import java.util.Arrays;
+import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Map.Entry;
 import java.util.Scanner;
 
 public class LongestPath {
@@ -10,23 +14,60 @@ public class LongestPath {
 
 		while (t > 0) {
 			int n = sc.nextInt();
-			LinkedList<Integer> adj[] = new LinkedList[n];
+			@SuppressWarnings("unchecked")
+			LinkedList<Hashtable<Integer, Integer>> adj[] = new LinkedList[n + 1];
+			boolean[] visited = new boolean[n + 1];
 
-			for (int i = 0; i < n; ++i) {
-				adj[i] = new LinkedList<Integer>();
+			for (int i = 1; i <= n; i++) {
+				adj[i] = new LinkedList<Hashtable<Integer, Integer>>();
+				Hashtable<Integer, Integer> pairs = new Hashtable<Integer, Integer>();
+				adj[i].add(pairs);
 			}
 
 			int e = sc.nextInt();
 
-			for (int i = 0; i < e; i++) {
-				int node1 = sc.nextInt() - 1;
-				int node2 = sc.nextInt() - 1;
+			for (int i = 1; i <= e; i++) {
+				int node1 = sc.nextInt();
+				int node2 = sc.nextInt();
 				int w = sc.nextInt();
-				adj[node1].add(w);
-				adj[node2].add(w);
+				adj[node1].get(0).put(node2, w);
+				adj[node2].get(0).put(node1, w);
 			}
+			int max = 0;
+			for (int i = 1; i <= n; i++) {
+				Arrays.fill(visited, false);
+				int tmp = findLongest(i, adj, visited, 0, 0);
+				if (max < tmp) {
+					max = tmp;
+				}
+			}
+
+			System.out.println(max);
 			t--;
 		}
 		sc.close();
+	}
+
+	static int findLongest(int node, LinkedList<Hashtable<Integer, Integer>>[] adj, boolean[] visited, int max,
+			int prev) {
+		int tmp = 0;
+		visited[node] = true;
+
+		Iterator<Entry<Integer, Integer>> i = adj[node].get(0).entrySet().iterator();
+
+		while (i.hasNext()) {
+			Entry<Integer, Integer> it = i.next();
+			int n = it.getKey();
+			if (!visited[n]) {
+				tmp = prev + it.getValue();
+				max = maximum(tmp, findLongest(n, adj, visited, max, tmp));
+			}
+			tmp = 0;
+		}
+		return max;
+	}
+
+	static int maximum(int a, int b) {
+		return a > b ? a : b;
 	}
 }
