9f13ab5165dde28d88e0f4092572bdc28397fdbf
Emelie Widegren <emelie.widegren@chalmers.se>
2016-10-30 19:37:20 +0100

    Q 2.2
    An iterative solution.

--- src/Chapter2/Q2.java
+++ src/Chapter2/Q2.java
@@ -32,4 +32,30 @@ public class Q2 {
 		}
 		return k;
 	}
+	
+	public static Node nthToLast(Node head, int n) {
+		Node p1 = head;
+		Node p2 = head;
+		
+		if (n <= 0) return null;
+		
+		// Move p2 n nodes into the list.  Keep n1 in the same position.
+		for (int i = 0; i < n - 1; i++) { 
+			if (p2 == null) {
+				return null; // Error: list is too small.
+			}
+			p2 = p2.next;
+		}
+		if (p2 == null) { // Another error check.
+			return null;
+		}
+		
+		// Move them at the same pace.  When p2 hits the end, 
+		// p1 will be at the right element.
+		while (p2.next != null) {
+			p1 = p1.next;
+			p2 = p2.next;
+	  	}
+	  	return p1;
+	}
 }
