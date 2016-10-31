96f6692c997c64b2a2ae73851e8759069c8caae1
Emelie Widegren <emelie.widegren@chalmers.se>
2016-10-31 18:50:55 +0100

    Q1.3
    Improved solution

--- src/Chapter1/Q3.java
+++ src/Chapter1/Q3.java
@@ -26,10 +26,29 @@ public class Q3 {
 			return false;
 		}
 	}
+	public static boolean permutation2(String str1, String str2){
+		if(str1.length() != str2.length()){
+			return false;
+		}
+		char[] chars = new char[256];
+		
+		for(char c : str1.toCharArray()){
+			chars[c]++;
+		}
+		for(int i = 0 ; i < str2.length(); i++){
+			char c = str2.charAt(i);
+			if(--chars[c] < 0){
+				return false;
+			}
+		}
+		return true;
+	}
+	
 	public static void main(String[] args){
 		String[] words = {"abc", "bca", "ab", "ba", "a"};
 		for (int i = 0; i < words.length-1;i++) {
 			System.out.println(words[i] + " " + words[i+1] + ": " + permutation(words[i], words[i+1]));
+			System.out.println(words[i] + " " + words[i+1] + ": " + permutation2(words[i], words[i+1]));
 		}
 	}
 }
