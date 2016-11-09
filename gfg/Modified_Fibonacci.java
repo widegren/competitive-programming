bfac6d346d3586daec46a5c79dee2df1f1a19bfb
Emelie Widegren <emelie.widegren@chalmers.se>
2016-11-10 00:15:58 +0100

    optimisation

--- gfg/Modified_Fibonacci.java
+++ gfg/Modified_Fibonacci.java
@@ -3,23 +3,27 @@ package gfg;
 import java.util.Scanner;
 
 public class Modified_Fibonacci {
-	public static void main(String[] args ){
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int t = sc.nextInt();
-		while(t>0){
+		while (t > 0) {
 			int n = sc.nextInt();
-			int[] fib = new int[n+1];
-			int A = sc.nextInt();
-			int B = sc.nextInt();
-			int C = sc.nextInt();
-			
-			fib[1] = A+B;
-			fib[2] = B+C;
-			
-			for(int i = 3; i <= n ; i++){
-				fib[i] = fib[i-1] - fib[i-2];
+			long[] fib = new long[6];
+			long A = (long) sc.nextInt();
+			long B = (long) sc.nextInt();
+			long C = (long) sc.nextInt();
+
+			fib[0] = A + B;
+			fib[1] = B + C;
+
+			for (int i = 2; i < 6; i++) {
+				fib[i] = fib[i - 1] - fib[i - 2];
 			}
-			System.out.println(fib[n] % 1000000007);
+			long f = fib[(int) ((n - 1) % 6)];
+			while (f < 0)
+				f = f + 1000000007;
+			System.out.println(f % 1000000007);
+
 			t--;
 		}
 		sc.close();
