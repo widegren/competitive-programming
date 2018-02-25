f223540f0c0ec4e5802c6cf6d5b4660881e98076
Emelie Widegren <emelie.widegren@chalmers.se>
2018-02-25 21:07:07 +0100

    Problem 1 in Python

---
+++ Python/problem1.py
@@ -0,0 +1,10 @@
+
+def multiples():
+    sum = 0
+    for i in range(1000):
+        if (i % 3) == 0 or (i % 5) == 0:
+            sum += i
+    return sum
+
+if __name__ == '__main__':
+    print(multiples())
