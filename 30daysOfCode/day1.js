33d4980b2a0bee309b25252ff4f4f677c755734f
Emelie Widegren <emelie.widegren@chalmers.se>
2018-12-28 00:24:43 +0100

    Add some space

--- 30daysOfCode/day1.js
+++ 30daysOfCode/day1.js
@@ -33,14 +33,18 @@ function main () {
   var secondInteger = 1
   var secondDouble = 1.0
   var secondStr = '1'
+
   // Read and save an integer, double, and String to your variables.
   secondInteger = parseInt(readLine())
   secondDouble = parseFloat(readLine())
   secondStr = readLine()
+
   // Print the sum of both integer variables on a new line.
   console.log(i + secondInteger)
+
   // Print the sum of the double variables on a new line.
   console.log((d + secondDouble).toFixed(1))
+
   // Concatenate and print the String variables on a new line
   // The 's' variable above should be printed first.
   console.log(s + secondStr)
