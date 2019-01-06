0f0f87e322a264331d67ba0bc4fe486c3decf518
Emelie Widegren <emelie.widegren@chalmers.se>
2019-01-06 20:15:50 +0100

    Add day10

---
+++ 30daysOfCode/day10.js
@@ -0,0 +1,31 @@
+'use strict'
+
+process.stdin.resume()
+process.stdin.setEncoding('utf-8')
+
+let inputString = ''
+let currentLine = 0
+
+process.stdin.on('data', inputStdin => {
+  inputString += inputStdin
+})
+
+process.stdin.on('end', _ => {
+  inputString = inputString.replace(/\s*$/, '')
+    .split('\n')
+    .map(str => str.replace(/\s*$/, ''))
+
+  main()
+})
+
+function readLine () {
+  return inputString[currentLine++]
+}
+
+function main () {
+  const n = parseInt(readLine(), 10).toString(2)
+  let splits = n.split('0')
+  console.log(splits.map(function (elem) { return elem.length }).reduce(function (a, b) {
+    if (a > b) return a; else return b
+  }))
+}
