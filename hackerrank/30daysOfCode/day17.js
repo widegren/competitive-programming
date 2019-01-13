2f8af33ad3743ef5b7477fba7f99f63baaa743f9
Emelie Widegren <emelie.widegren@chalmers.se>
2019-01-13 13:52:53 +0100

    Add day17

---
+++ hackerrank/30daysOfCode/day17.js
@@ -0,0 +1,42 @@
+process.stdin.resume()
+process.stdin.setEncoding('ascii')
+
+var inputStdin = ''
+var inputStdinArray = ''
+var inputCurrentline = 0
+
+process.stdin.on('data', function (data) {
+  inputStdin += data
+})
+
+process.stdin.on('end', function () {
+  inputStdinArray = inputStdin.split('\n')
+  main()
+})
+function readLine () {
+  return inputStdinArray[inputCurrentline++]
+}
+
+class Calculator {
+  power (n, p) {
+    if (n < 0 || p < 0) {
+      return 'n and p should be non-negative'
+    } else return n ** p
+  }
+}
+
+function main () {
+  var myCalculator = new Calculator()
+  var T = parseInt(readLine())
+  while (T-- > 0) {
+    var num = (readLine().split(' '))
+    try {
+      var n = parseInt(num[0])
+      var p = parseInt(num[1])
+      var ans = myCalculator.power(n, p)
+      console.log(ans)
+    } catch (e) {
+      console.log(e)
+    }
+  }
+}
