d02ea5d654bdf82accd99440ae6d0bac459cb6a7
Emelie Widegren <emelie.widegren@chalmers.se>
2019-01-27 14:09:30 +0100

    Fix linting

--- advent-of-code/2016/day11.py
+++ advent-of-code/2016/day11.py
@@ -5,6 +5,6 @@ Python solution for Advent of Code Day 11: Radioisotope Thermoelectric Generator
 __author__ = "Emelie Widegren"
 __email__ = "emeliewide@gmail.com"
 
-
-print(sum(2 * sum([2,4,4,0][:x]) for x in range(1,4))-3)
-print(sum(2 * sum([6,4,4,0][:x]) for x in range(1,4))-3)
+if __name__ == '__main__':
+    print(sum(2 * sum([2, 4, 4, 0][:x]) for x in range(1, 4)) - 3)
+    print(sum(2 * sum([6, 4, 4, 0][:x]) for x in range(1, 4)) - 3)
