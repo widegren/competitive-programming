5226031f7b40cb54ddb59ea4161551515240f79b
Emelie Widegren <emelie.widegren@chalmers.se>
2019-01-29 14:07:15 +0100

    Add module docstring

--- advent-of-code/2016/Day11.py
+++ advent-of-code/2016/Day11.py
@@ -1,2 +1,10 @@
-print sum(2 * sum([2,4,4,0][:x]) for x in range(1,4))-3
-print sum(2 * sum([6,4,4,0][:x]) for x in range(1,4))-3
+#!/usr/bin/env python
+"""
+Python solution for Advent of Code Day 11: Radioisotope Thermoelectric Generators
+"""
+__author__ = "Emelie Widegren"
+__email__ = "emeliewide@gmail.com"
+
+
+print(sum(2 * sum([2,4,4,0][:x]) for x in range(1,4))-3)
+print(sum(2 * sum([6,4,4,0][:x]) for x in range(1,4))-3)
