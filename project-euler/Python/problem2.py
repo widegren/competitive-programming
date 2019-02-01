7bc5c9f9a4ccd3d3a1c859bf5289a5c5c6592dd0
Emelie Widegren <emelie.widegren@chalmers.se>
2019-02-01 14:01:22 +0100

    Add docstrings

--- project-euler/Python/problem2.py
+++ project-euler/Python/problem2.py
@@ -1,12 +1,23 @@
+#!/usr/bin/env python
+"""
+Python solution for Project Euler Problem 2: Even Fibonacci numbers
+"""
+__author__ = "Emelie Widegren"
+__email__ = "emeliewide@gmail.com"
+
 
 def even_fibonacci():
-	a, b = 1, 1
-	sum = 0
-	while b < 4000000:
-		a, b = b, a+b
-		if b % 2 == 0:
-			sum += b
-	return sum
+    """
+    Returns the sum of the even-valued terms below four million.
+    """
+    a, b = 1, 1
+    sum = 0
+    while b < 4000000:
+        a, b = b, a + b
+        if b % 2 == 0:
+            sum += b
+    return sum
+
 
 if __name__ == '__main__':
     print(even_fibonacci())
