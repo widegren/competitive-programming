b53bf1dd26852fa6f6b24c2b92b631b8cc4e6440
Emelie Widegren <emelie.widegren@chalmers.se>
2019-02-02 13:57:54 +0100

    Fix linting

--- project-euler/Python/problem1.py
+++ project-euler/Python/problem1.py
@@ -1,12 +1,21 @@
+#!/usr/bin/env python
+"""
+Python solution for Project Euler Problem 1: Multiples of 3 and 5
+"""
+__author__ = "Emelie Widegren"
+__email__ = "emeliewide@gmail.com"
+
 
 def multiples():
-    sum = 0
+    """
+    Returns the sum of all the multiples of 3 or 5 below 1000.
+    """
+    result = 0
     for i in range(1000):
         if (i % 3) == 0 or (i % 5) == 0:
-            sum += i
-    return sum
+            result += i
+    return result
+
 
 if __name__ == '__main__':
     print(multiples())
-
-    #Hej söt
