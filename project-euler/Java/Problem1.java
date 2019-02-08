8d14d7948444f61c38474ed1c265316861d21991
Emelie Widegren <emelie.widegren@chalmers.se>
2019-02-08 21:46:35 +0100

    Update solution

--- project-euler/Java/Problem1.java
+++ project-euler/Java/Problem1.java
@@ -1,17 +1,22 @@
-
+/**
+ * Java solution for Project Euler Problem 1: Multiples of 3 and 5
+ *
+ * @author Emelie Widegren <widegren.emelie@gmail.com>
+ */
 public class Problem1 {
 
-	public static void main(String arg[]) {
-		problem1();
-	}
+  public static void main(String arg[]) {
+    System.out.println(multiples());
+  }
 
-	public static void problem1() {
-		int result = 0;
-		for (int i = 0; i < 1000; i++) {
-			if (i % 3 == 0 || i % 5 == 0) {
-				result = result + i;
-			}
-		}
-		System.out.println(result);
-	}
+  /** Returns the sum of all the multiples of 3 or 5 below 1000. */
+  private static int multiples() {
+    int result = 0;
+    for (int i = 0; i < 1000; i++) {
+      if (i % 3 == 0 || i % 5 == 0) {
+        result += i;
+      }
+    }
+    return result;
+  }
 }
