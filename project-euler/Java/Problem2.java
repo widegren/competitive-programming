4718929e64dfd4bad73e17e915ffb64fff2bdcab
Emelie Widegren <emelie.widegren@chalmers.se>
2019-02-09 21:47:04 +0100

    Update solution

--- project-euler/Java/Problem2.java
+++ project-euler/Java/Problem2.java
@@ -1,26 +1,31 @@
 import java.util.ArrayList;
 
+/**
+ * Java solution for Project Euler Problem 2: Even Fibonacci numbers
+ *
+ * @author Emelie Widegren <widegren.emelie@gmail.com>
+ */
 public class Problem2 {
 
-	public static void main(String arg[]) {
-		problem2();
-	}
+  public static void main(String arg[]) {
+    evenFibonacci();
+  }
 
-	public static void problem2() {
-		ArrayList<Integer> list = new ArrayList<Integer>();
-		int result = 0, i = 0, fib = 1;
+  /** Returns the sum of the even-valued terms below four million. */
+  private static void evenFibonacci() {
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    int result = 0, i = 0, fib = 1;
 
-		while (fib < 4000000) {
-			list.add(fib);
-			if (i != 0) {
-				fib = fib + list.get(i - 1);
-				if (fib % 2 == 0) {
-					result = result + fib;
-				}
-			}
-			i++;
-		}
-		System.out.println(result);
-	}
-
+    while (fib < 4000000) {
+      list.add(fib);
+      if (i != 0) {
+        fib = fib + list.get(i - 1);
+        if (fib % 2 == 0) {
+          result = result + fib;
+        }
+      }
+      i++;
+    }
+    System.out.println(result);
+  }
 }
